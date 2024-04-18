e90424734ec0cff7637f95fd70baf4a22fcb23d3
yagooligeo <yagooliveira485@gmail.com>
2024-04-18 18:15:18 -0300

    nova tentativa commit

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 console.log("ola mundao")
+// classificador nivel heroi desafio felipao 
+
+console.log("Digite o nome do seu jogador")
+
+//declaracao de variavel 
+let nickname = "MrYagugo"
+
+console.log("Bem vindo " + nickname)
+console.log(nickname + "entrou no servidor")
+
+let nivelXp = 7300
+
+// descrição dos niveis de XP 
+
+
+{
+    if (nivelXp <= 1000)
+    classif = ferro;
+} 
+    if (xp >= 1001 && xp <= 2000) 
+{
+    classif = "Bronze";
+
+} else if (xp >= 2001 && xp <= 6000) {
+    classif = "Prata";
+
+} else if (xp >= 6001 && xp <= 7000) {
+    classif = "Ouro";
+
+} else if (xp >= 7001 && xp <= 8000) {
+    classif = "Platina";
+
+} else if (xp >= 8001 && xp <= 9000) {
+    classif = "Ascedente";
+
+} else if (xp >= 9001 && xp <= 10000) {
+    classif = "Imortal";
+
+} else {
+    classif = "Radiante";
+} 
+
+console.log("O heroi " + nickname + "esta no nivel" + nivelXp)
